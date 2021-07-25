a00318899ea3e4215a6352f2abe83692a6751f3f
Anon Ymus <beaver700nh@gmail.com>
2021-07-25 16:01:49 -0400

    added chest remover

--- factions/init.lua
+++ factions/init.lua
@@ -341,3 +341,37 @@ minetest.register_craft({
     recipe = "factions:chest",
     burntime = 30,
 })
+
+local chest_remover_toolcaps = {
+    full_punch_interval = 0.1,
+    max_drop_level = 3,
+    groupcaps = {
+	      unbreakable = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+	      fleshy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+	      choppy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+	      bendy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+	      cracky = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+	      crumbly = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+	      snappy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+    },
+    damage_groups = {fleshy = 1000},
+}
+
+minetest.register_tool("factions:chest_remover", {
+	  description = "Chest Remover (breaks non-empty chests)",
+	  range = 100,
+	  inventory_image = "factions_chest_remover.png",
+	  groups = {not_in_creative_inventory = 1},
+	  tool_capabilities = chest_remover_toolcaps,
+})
+
+minetest.register_on_punchnode(function(pos, node, puncher)
+	  if puncher:get_wielded_item():get_name() == "factions:chest_remover"
+	  and minetest.get_node(pos).name ~= "air" then
+		    -- The node is removed directly, which means it even works
+		    -- on non-empty containers and group-less nodes
+		    minetest.remove_node(pos)
+		    -- Run node update actions like falling nodes
+		    minetest.check_for_falling(pos)
+	  end
+end)
