cc4b8fc0daa662c74dea1c7e84ca2d4e3549feca
Anon Ymus <beaver700nh@gmail.com>
2021-08-04 19:16:06 -0400

    Added comments

--- init.lua
+++ init.lua
@@ -1,6 +1,23 @@
+----------------------------------------------
+----------------------------------------------
+------ 'FACTIONS' MOD FOR MINETEST GAME ------
+----------------------------------------------
+----------------------------------------------
+---- see license.txt for more information ----
+----------------------------------------------
+----------------------------------------------
+
+--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
+
+-------------------------
+---- Misc init stuff ----
+-------------------------
+
 local gen_def = dofile(minetest.get_modpath("factions") .. "/utils/base.lua")
 local actions = dofile(minetest.get_modpath("factions") .. "/utils/actions.lua")
+
 factions = {}
+
 local storage = minetest.get_mod_storage()
 if minetest.deserialize(storage:get_string("player_factions")) then
     factions.player_factions = minetest.deserialize(storage:get_string("player_factions"))
@@ -8,14 +25,17 @@ else
     factions.player_factions = {}
     storage:set_string("player_factions", minetest.serialize({}))
 end
+
 function factions.get_player_faction(username)
     return factions.player_factions[username]
 end
+
 function factions.set_player_faction(username, faction)
    factions.player_factions[username] = faction
    storage:set_string("player_factions", minetest.serialize(factions.player_factions))
 end
-local function has_value (tab, val)
+
+local function has_value(tab, val)
     for index, value in ipairs(tab) do
         if value == val then
             return true
@@ -25,12 +45,15 @@ local function has_value (tab, val)
     return false
 end
 
-local function print_all_of (tab)
+local function print_all_of(tab)
     for index, value in ipairs(tab) do
         print(value)
     end
 end
 
+-----------------------
+---- Chat commands ----
+-----------------------
 
 minetest.register_chatcommand("add_faction", {
     params = "<faction name>",
@@ -40,73 +63,76 @@ minetest.register_chatcommand("add_faction", {
     },
     description = "Allows admins/moderators to add factions",
     func = function(username, params)
-          if minetest.deserialize(storage:get_string("factions")) then
-              local facs = minetest.deserialize(storage:get_string("factions"))
-              facs[#facs+1] = params
-              print_all_of(facs)
+        if minetest.deserialize(storage:get_string("factions")) then
+            local facs = minetest.deserialize(storage:get_string("factions"))
+            facs[#facs+1] = params
+            print_all_of(facs)
 
-              print(minetest.serialize(facs))
+            print(minetest.serialize(facs))
 
-              storage:set_string("factions", minetest.serialize(facs))
+            storage:set_string("factions", minetest.serialize(facs))
 
-              local x = minetest.deserialize(storage:get_string("faction_color"))
-              if not x then
-                  x = {}
-              end
-              x[params] = {
-                  r = 255,
-                  b = 255,
-                  g = 255
-              }
-              storage:set_string("faction_color", minetest.serialize(x))
+            local x = minetest.deserialize(storage:get_string("faction_color"))
+            if not x then
+                x = {}
+            end
+
+            x[params] = {
+                r = 255,
+                b = 255,
+                g = 255
+            }
+
+            storage:set_string("faction_color", minetest.serialize(x))
         else
             storage:set_string("factions", minetest.serialize({params}))
-            local x = minetest.deserialize(storage:get_string("faction_color"))
-              if not x then
-                  x = {}
-              end
-              x[params] = {
-                  r = 255,
-                  b = 255,
-                  g = 255
-              }
 
-          end
+            local x = minetest.deserialize(storage:get_string("faction_color"))
+            if not x then
+                x = {}
+            end
+
+            x[params] = {
+                r = 255,
+                b = 255,
+                g = 255
+            }
+        end
     end
 })
 
 minetest.register_chatcommand("invite_to_faction", {
     params = "<faction name>",
     privs = {
-          interact = true,
-          faction_leader = true
+        interact = true,
+        faction_leader = true
     },
     description = "Invite a player into a faction",
     func = function(username, param)
-          local user = minetest.get_player_by_name(username)
-          local player = minetest.get_player_by_name(param)
+        local user = minetest.get_player_by_name(username)
+        local player = minetest.get_player_by_name(param)
 
-          if not player then
-                minetest.chat_send_player(user:get_player_name(), "That player does not exist or is not online")
-          else
-                if player:get_attribute("factions") then
-                      local facs = minetest.deserialize(player:get_attribute("factions"))
+        if not player then
+            minetest.chat_send_player(user:get_player_name(), "That player does not exist or is not online")
+        else
+            if player:get_attribute("factions") then
+                local facs = minetest.deserialize(player:get_attribute("factions"))
+                facs[#facs+1] = user:get_attribute("faction")
+                print_all_of(facs)
 
-                      facs[#facs+1] = user:get_attribute("faction")
-                      print_all_of(facs)
-                      player:set_attribute("factions", minetest.serialize(facs))
-                else
-                      player:set_attribute("factions", minetest.serialize({user:get_attribute("faction")}))
-                end
-          end
+                player:set_attribute("factions", minetest.serialize(facs))
+            else
+                player:set_attribute("factions", minetest.serialize({user:get_attribute("faction")}))
+            end
+        end
     end
 })
 
 minetest.register_chatcommand("join_faction", {
     params = "<faction name>",
     privs = {
-          interact = true,
-          faction_leader = false
+        interact = true,
+        faction_leader = false
     },
     description = "Join a faction you are allowed into",
     func = function(username, param)
@@ -117,20 +143,23 @@ minetest.register_chatcommand("join_faction", {
         end
 
         if has_value(minetest.deserialize(user:get_attribute("factions")), param) then
-              user:set_attribute("faction", param)
+            user:set_attribute("faction", param)
         else
             return false, "You aren't allowed to join that faction."
         end
 
         local nick = user:get_attribute("faction")
         factions.set_player_faction(username, nick)
+
         if not x then
             x = {}
+
             x[user:get_attribute("faction")] = {
                 r = 255,
                 b = 255,
                 g = 255
             }
+
             storage:set_string("faction_color", minetest.serialize(x))
         end
 
@@ -145,8 +174,8 @@ minetest.register_chatcommand("join_faction", {
 minetest.register_chatcommand("set_faction_color", {
     params = "<red> <green> <blue>",
     privs = {
-      interact = true,
-      faction_leader = true
+        interact = true,
+        faction_leader = true
     },
     description = "Set the color of a faction",
     func = function(username, param)
@@ -224,11 +253,13 @@ minetest.register_chatcommand("set_faction", {
         end
 
         local nick = player:get_attribute("faction")
-	factions.set_player_faction(to, nick)
+        factions.set_player_faction(to, nick)
+
         local x = minetest.deserialize(storage:get_string("faction_color"))
 
         if not x then
             x = {}
+
             x[player:get_attribute("faction")] = {
                 r = 255,
                 g = 255,
@@ -289,6 +320,10 @@ minetest.register_privilege("set_faction", {
     give_to_singleplayer = false
 })
 
+--------------------------------------------------------
+---- Assign players to 'neutral' faction by default ----
+--------------------------------------------------------
+
 minetest.register_on_joinplayer(function(player)
     if not player:get_attribute("faction") then
         player:set_attribute("faction", "neutral")
@@ -299,79 +334,101 @@ minetest.register_on_joinplayer(function(player)
     end
 
     local nick = player:get_attribute("faction")
+
     if not factions.player_factions[player:get_player_name()] then
         factions.player_factions[player:get_player_name()] = nick
         storage:set_string("player_factions", minetest.serialize(factions.player_factions))
     end
+
     local x = minetest.deserialize(storage:get_string("faction_color"))
+
     if not x then
         x = {}
+
         x[player:get_attribute("faction")] = {
             r = 255,
             b = 255,
             g = 255
         }
-        storage:set_string("faction_color", minetest.serialize(x))
 
+        storage:set_string("faction_color", minetest.serialize(x))
     end
+
     local colors = x[player:get_attribute("faction")]
+
     if nick then
         player:set_nametag_attributes({text = "(" .. nick .. ")" .. " " .. player:get_player_name(), color = colors})
     end
 end) 
+
+------------------------------------------
+---- Faction chest functionality code ----
+------------------------------------------
+
 function has_locked_chest_privilege(meta, player)
-	if player:get_player_name() ~= meta:get_string("faction_owner") then
-		return false
-	end
-	return true
+    if player:get_player_name() ~= meta:get_string("faction_owner") then
+        return false
+    end
+
+    return true
 end
+
 local chest = gen_def({
-	description = "Factions chest",
-	type = "chest",
-	size = "small",
-	tiles = {
+    description = "Factions chest",
+    type = "chest",
+    size = "small",
+    tiles = {
         top = "factions_chest_top.png",
         side = "factions_chest_right.png",
-
         front ="factions_chest_front.png"
     },
-	pipeworks_enabled = false,
-	allow_metadata_inventory_move = false,
-	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-		local meta = minetest.get_meta(pos)
-		if actions.has_locked_chest_privilege(meta, player) then
-			return stack:get_count()
-		end
-		local target = meta:get_inventory():get_list(listname)[index]
-		local target_name = target:get_name()
-		local stack_count = stack:get_count()
-		if target_name == stack:get_name()
-		and target:get_count() < stack_count then
-			return stack_count
-		end
-		if target_name ~= "" then
-			return 0
-		end
-		return stack_count
-	end,
-	allow_metadata_inventory_take = actions.get_allow_metadata_inventory_take({
-		"dropbox", check_privs = actions.has_locked_chest_privilege
-	}),
+    pipeworks_enabled = false,
+    allow_metadata_inventory_move = false,
+    allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+        local meta = minetest.get_meta(pos)
+
+        if actions.has_locked_chest_privilege(meta, player) then
+            return stack:get_count()
+        end
+
+        local target = meta:get_inventory():get_list(listname)[index]
+        local target_name = target:get_name()
+        local stack_count = stack:get_count()
+
+        if target_name == stack:get_name()
+        and target:get_count() < stack_count then
+            return stack_count
+        end
+
+        if target_name ~= "" then
+            return 0
+        end
+
+        return stack_count
+    end,
+    allow_metadata_inventory_take = actions.get_allow_metadata_inventory_take({
+        "dropbox", check_privs = actions.has_locked_chest_privilege
+    }),
 })
 
+---------------------------
+---- Registering nodes ----
+---------------------------
+
+-- Factions Chest
 minetest.register_node("factions:chest", chest)
 
-minetest.register_craft({
-    output = "factions:chest",
+minetest.register_craft({ -- Like the normal craft but logs instead of planks
+    output = "factions:chest", -- And there's a mese crystal in the middle
     recipe = {
-        {"group:tree", "group:tree", "group:tree"},
+        {"group:tree", "group:tree",           "group:tree"},
         {"group:tree", "default:mese_crystal", "group:tree"},
-        {"group:tree", "group:tree", "group:tree"},
+        {"group:tree", "group:tree",           "group:tree"},
     },
 })
 
-minetest.register_craft({
-    output = "factions:chest",
+minetest.register_craft({ -- Or you can combine 4 normal chests
+    output = "factions:chest", -- around a mese crystal
     recipe = {
         {"",              "default:chest",        ""},
         {"default:chest", "default:mese_crystal", "default:chest"},
@@ -379,13 +436,19 @@ minetest.register_craft({
     },
 })
 
-minetest.register_craft({
-    type = "fuel",
+minetest.register_craft({ -- Factions chests can be used as fuel in a furnace
+    type = "fuel", -- with the same burn time as a normal default:chest
     recipe = "factions:chest",
     burntime = 30,
 })
 
-local chest_remover_toolcaps = {
+--------------------------------------------------------------
+---- Chest remover tool (instamine pickaxe, removes nodes ----
+---- by calling minetest.remove_node(), so it can even    ----
+---- remove non-empty chests which you can't do normally) ----
+--------------------------------------------------------------
+
+local chest_remover_toolcaps = { -- Everything takes 0 seconds to mine
     full_punch_interval = 0.1,
     max_drop_level = 3,
     groupcaps = {
@@ -397,7 +460,7 @@ local chest_remover_toolcaps = {
 	      crumbly = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
 	      snappy = {times = {[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
     },
-    damage_groups = {fleshy = 1000},
+    damage_groups = {fleshy = 1000}, -- 1000 damage - also a very powerful weapon
 }
 
 minetest.register_tool("factions:chest_remover", {
