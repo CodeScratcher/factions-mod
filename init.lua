c9cec6bd7d4efc8bf4055203807a67afbc295d3b
Arielle F <bb8andr2d2forlife@gmail.com>
2022-08-24 07:16:34 -0400

    Added more settings

--- init.lua
+++ init.lua
@@ -586,6 +586,9 @@ local function rgb_to_hex(rgb)
 end
 
 minetest.register_on_chat_message(function(name, message)
+    if (minetest.settings.no_chat_intercept) then
+        return false
+    end
     if (minetest.get_player_by_name(name)) then
         local x = minetest.deserialize(storage:get_string("faction_color"))
         local player = minetest.get_player_by_name(name)
